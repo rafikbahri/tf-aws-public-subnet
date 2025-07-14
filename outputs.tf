429f4996ebf211897c7e262e4fa1a8e77722197a
Rafik Bahri <rbahri@feedgy.solar>
2025-07-14 18:21:55 +0200

    ci: Add CI

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "arn" {
   description = "Subnet ARN"
   value       = aws_subnet.subnet.arn
 }
-
-output "cidr_block" {
-  description = "Subnet CIDR block"
-  value       = aws_subnet.subnet.cidr_block
-}
